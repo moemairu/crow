c136322662ba04c9bb7dd62415850a9d48d17962
moemairu <ismailfaruq.qy@gmail.com>
2026-06-28 12:31:08 +0700

    feat: mod directory scanner returns GListStore

--- src/main.c
+++ src/main.c
@@ -1,13 +1,28 @@
 #include <gtk/gtk.h>
 #include "config.h"
+#include "crow_mod.h"
 
 static void on_activate(GtkApplication *app, gpointer user_data) {
     (void)user_data;
 
-    /* Test config load */
+    /* Test config + scanner */
     gchar *ggst_path = crow_config_load();
     if (ggst_path) {
-        g_print("crow: loaded GGST path: %s\n", ggst_path);
+        g_print("crow: GGST path: %s\n", ggst_path);
+
+        GListStore *store = crow_mod_scan_directory(ggst_path);
+        if (store) {
+            guint n = g_list_model_get_n_items(G_LIST_MODEL(store));
+            g_print("crow: found %u mod(s)\n", n);
+            for (guint i = 0; i < n; i++) {
+                CrowMod *mod = g_list_model_get_item(G_LIST_MODEL(store), i);
+                g_print("  [%s] %s\n",
+                        crow_mod_get_enabled(mod) ? "ON " : "OFF",
+                        crow_mod_get_name(mod));
+                g_object_unref(mod);
+            }
+            g_object_unref(store);
+        }
         g_free(ggst_path);
     } else {
         g_print("crow: no GGST path configured\n");
